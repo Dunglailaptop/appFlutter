2dc2bd8a3f9709aef715d040d35b28eba88c8eca
DUNGCT\ndung <ndung983@gmail.com>
2025-03-17 20:41:55 +0700

    oke

--- appdoctor_nd2/lib/main.dart
+++ appdoctor_nd2/lib/main.dart
@@ -1,16 +1,63 @@
 import 'package:flutter/material.dart';
-import 'splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: SafeArea(child: Scaffold(body: MyWidget())),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+    return Text(
+      "nhưng bạn công ty vô cùng vui vẻ hòa đồng,"
+      "nhưng bạn công ty vô cùng vui vẻ hòa đồng,nhưng bạn công ty vô cùng vui vẻ hòa đồng",
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.justify,
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+    );
   }
 }
+
+//
+// class MyWidget extends StatefulWidget {
+//   final bool loading;
+//
+//   MyWidget(this.loading);
+//
+//   @override
+//   State<StatefulWidget> createState() {
+//     // TODO: implement build
+//     return MyWight2State();
+//   }
+// }
+//
+// class MyWight2State extends State<MyWidget> {
+//   late bool _localloading;
+//
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//     _localloading = widget.loading;
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return _localloading
+//         ? CircularProgressIndicator()
+//         : FloatingActionButton(onPressed: onClickButton);
+//   }
+//
+//   void onClickButton() {
+//     setState(() {
+//       _localloading = true;
+//     });
+//   }
+// }
