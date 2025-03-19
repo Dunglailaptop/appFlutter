21c2c9ddb2b610ea3f4a8b9aedb435423efdfdb9
DUNGCT\ndung <ndung983@gmail.com>
2025-03-19 21:44:02 +0700

    ok

--- appdoctor_nd2/lib/home.dart
+++ appdoctor_nd2/lib/home.dart
@@ -1,137 +1,68 @@
+import 'dart:convert'; // For JSON decoding
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // For loading assets
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<Home> {
+  List<Map<String, dynamic>> _allUsers = [];
+
+  // Method to load and parse the JSON data
+  Future<void> _loadJsonData() async {
+    // Load the data from the assets
+    String jsonString = await rootBundle.loadString(
+      'lib/JsonFile/ListKhoa.json',
+    );
+
+    // Decode the JSON data into a List
+    List<dynamic> jsonResponse = json.decode(jsonString);
+
+    // Set the state with the loaded data
+    setState(() {
+      _allUsers =
+          jsonResponse.map((item) {
+            return {"id": item["id"], "title": item["title"]};
+          }).toList();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadJsonData(); // Load the data when the widget is first created
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          constraints: const BoxConstraints(maxWidth: 400),
-          child: ListView.builder(
-            itemCount: _articles.length,
-            itemBuilder: (BuildContext context, int index) {
-              final item = _articles[index];
-              return Container(
-                height: 136,
-                margin:
-                const EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
-                decoration: BoxDecoration(
-                    border: Border.all(color: const Color(0xFFE0E0E0)),
-                    borderRadius: BorderRadius.circular(8.0)),
-                padding: const EdgeInsets.all(8),
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              item.title,
-                              style: const TextStyle(fontWeight: FontWeight.bold),
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            const SizedBox(height: 8),
-                            Text("${item.author} · ${item.postedOn}",
-                                style: Theme.of(context).textTheme.labelMedium),
-                            const SizedBox(height: 8),
-                            Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Icons.bookmark_border_rounded,
-                                Icons.share,
-                                Icons.more_vert
-                              ].map((e) {
-                                return InkWell(
-                                  onTap: () {},
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(right: 8.0),
-                                    child: Icon(e, size: 16),
-                                  ),
-                                );
-                              }).toList(),
-                            )
-                          ],
-                        )),
-                    Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(8.0),
-                            image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(item.imageUrl),
-                            ))),
-                  ],
+      appBar: AppBar(title: Text('Trang chủ')),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: _allUsers.length,
+        itemBuilder:
+            (context, index) => Card(
+              key: ValueKey(_allUsers[index]["id"]),
+              color: Colors.green,
+              elevation: 4,
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: ListTile(
+                leading: Text(
+                  _allUsers[index]["id"].toString(),
+                  style: const TextStyle(fontSize: 24, color: Colors.white),
                 ),
-              );
-            },
-          ),
-        ),
+                title: Text(_allUsers[index]["title"]),
+                titleTextStyle: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
       ),
     );
   }
 }
-
-class Article {
-  final String title;
-  final String imageUrl;
-  final String author;
-  final String postedOn;
-
-  Article(
-      {required this.title,
-        required this.imageUrl,
-        required this.author,
-        required this.postedOn});
-}
-
-final List<Article> _articles = [
-  Article(
-    title: "Instagram quietly limits ‘daily time limit’ option",
-    author: "MacRumors",
-    imageUrl: "https://picsum.photos/id/1000/960/540",
-    postedOn: "Yesterday",
-  ),
-  Article(
-      title: "Google Search dark theme goes fully black for some on the web",
-      imageUrl: "https://picsum.photos/id/1010/960/540",
-      author: "9to5Google",
-      postedOn: "4 hours ago"),
-  Article(
-    title: "Check your iPhone now: warning signs someone is spying on you",
-    author: "New York Times",
-    imageUrl: "https://picsum.photos/id/1001/960/540",
-    postedOn: "2 days ago",
-  ),
-  Article(
-    title:
-    "Amazon’s incredibly popular Lost Ark MMO is ‘at capacity’ in central Europe",
-    author: "MacRumors",
-    imageUrl: "https://picsum.photos/id/1002/960/540",
-    postedOn: "22 hours ago",
-  ),
-  Article(
-    title:
-    "Panasonic's 25-megapixel GH6 is the highest resolution Micro Four Thirds camera yet",
-    author: "Polygon",
-    imageUrl: "https://picsum.photos/id/1020/960/540",
-    postedOn: "2 hours ago",
-  ),
-  Article(
-    title: "Samsung Galaxy S22 Ultra charges strangely slowly",
-    author: "TechRadar",
-    imageUrl: "https://picsum.photos/id/1021/960/540",
-    postedOn: "10 days ago",
-  ),
-  Article(
-    title: "Snapchat unveils real-time location sharing",
-    author: "Fox Business",
-    imageUrl: "https://picsum.photos/id/1060/960/540",
-    postedOn: "10 hours ago",
-  ),
-];
