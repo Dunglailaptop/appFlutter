867bc6ad6795bfa3897489df0273f9ad0a919d80
NguyenXuantiendung <101032317+Dunglailaptop@users.noreply.github.com>
2025-03-19 16:20:40 +0700

    ok

---
+++ appdoctor_nd2/lib/login.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:appdoctor_nd2/home.dart';
+
+class SignInPage2 extends StatelessWidget {
+  const SignInPage2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isSmallScreen = MediaQuery.of(context).size.width < 600;
+
+    return Scaffold(
+        body: Center(
+            child: isSmallScreen
+                ? Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                _Logo(),
+                _FormContent(),
+              ],
+            )
+                : Container(
+              padding: const EdgeInsets.all(32.0),
+              constraints: const BoxConstraints(maxWidth: 800),
+              child: Row(
+                children: const [
+                  Expanded(child: _Logo()),
+                  Expanded(
+                    child: Center(child: _FormContent()),
+                  ),
+                ],
+              ),
+            )));
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isSmallScreen = MediaQuery.of(context).size.width < 600;
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset("Images/assets/icon.png"),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            "KHÁM BỆNH NỘI TRÚ",
+            textAlign: TextAlign.center,
+            style: isSmallScreen
+                ? Theme.of(context).textTheme.headlineLarge
+                : Theme.of(context)
+                .textTheme
+                .headlineLarge
+                ?.copyWith(color: Colors.black),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _FormContent extends StatefulWidget {
+  const _FormContent({Key? key}) : super(key: key);
+
+  @override
+  State<_FormContent> createState() => __FormContentState();
+}
+
+class __FormContentState extends State<_FormContent> {
+  bool _isPasswordVisible = false;
+  bool _rememberMe = false;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 300),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Vui lòng nhập';
+                }
+
+                if (value.length < 6) {
+                  return 'Tên đăng nhập phải ít nhất 5 ký tự';
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                labelText: 'Tên đăng nhập',
+                hintText: 'Nhập tên đăng nhập',
+                prefixIcon: Icon(Icons.email_outlined),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            _gap(),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Vui lòng nhập';
+                }
+
+                if (value.length < 6) {
+                  return 'Tên đăng nhập phải ít nhất 6 ký tự';
+                }
+                return null;
+              },
+              obscureText: !_isPasswordVisible,
+              decoration: InputDecoration(
+                  labelText: 'Mật khẩu',
+                  hintText: 'Nhập mật khẩu của bạn',
+                  prefixIcon: const Icon(Icons.lock_outline_rounded),
+                  border: const OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(_isPasswordVisible
+                        ? Icons.visibility_off
+                        : Icons.visibility),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  )),
+            ),
+            _gap(),
+            CheckboxListTile(
+              value: _rememberMe,
+              onChanged: (value) {
+                if (value == null) return;
+                setState(() {
+                  _rememberMe = value;
+                });
+              },
+              title: const Text('Ghi nhớ tài khoản'),
+              controlAffinity: ListTileControlAffinity.leading,
+              dense: true,
+              contentPadding: const EdgeInsets.all(0),
+            ),
+            _gap(),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4)),
+                  backgroundColor: Colors.green
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "ĐĂNG NHẬP",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold,color: Colors.white),
+                  ),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState?.validate() ?? false) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home()),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _gap() => const SizedBox(height: 16);
+}
